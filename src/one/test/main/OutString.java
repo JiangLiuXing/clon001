dac2ae25d83ab5b6e66a387fa9ffb1211a7f5b71
jianglx <13916079484@163.com>
2019-08-02 17:38:23 +0800

    Initial commit

---
+++ src/one/test/main/OutString.java
@@ -0,0 +1,11 @@
+package one.test.main;
+
+/**
+ * @author jlx
+ * @create 2019/08/02 17:37
+ */
+public class OutString {
+    public static void main(String[] args) {
+        System.out.println("测试一个项目的提交是否成功！");
+    }
+}
